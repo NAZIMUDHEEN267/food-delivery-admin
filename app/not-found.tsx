f062f35b52bce7e8a1c2b6bfa10a8fb9b95485bb
nazimudheen-lifisin <Fe02@lifisin.com>
2025-05-16 13:10:53 +0530

    code updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,20 @@
-'use client';
-
 import CustomButton from "@/components/CustomButton"
 import Image from "next/image"
-import { useRouter } from "next/navigation"
+// import { useRouter } from "next/navigation"
 
 function ErrorPage() {
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  const onClick = () => {
-    router.push('/')
-  }
+  // const onClick = () => {
+  //   router.push('/')
+  // }
 
   return (
     <div className="flex items-center flex-col justify-center h-screen bg-white">
       <Image src='/images/not-found.jpg' width={300} height={300} alt="not-found.png" />
       <div className="w-[300px]">
-        <CustomButton title="Back to home" onClick={onClick} />
+        {/* <CustomButton title="Back to home" onClick={onClick} /> */}
       </div>
     </div>
   )
